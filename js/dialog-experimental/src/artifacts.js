cc48a2375dabb5cf00df38f5e1300daede8d0204
Irakli Gozalishvili <contact@gozala.io>
2025-05-07 12:44:23 -0700

    fix: update to new api

--- js/dialog-experimental/src/artifacts.js
+++ js/dialog-experimental/src/artifacts.js
@@ -2,6 +2,7 @@ import init, {
   Artifacts,
   ValueDataType,
   InstructionType,
+  IDBStore,
 } from '@dialog-db/artifacts'
 import * as ArtifactsLib from '@dialog-db/artifacts'
 import { Task, API, Constant } from '@dialog-db/query'
@@ -28,9 +29,9 @@ export function* open(address) {
     initialized = true
   }
 
-  const artifact = yield* Task.wait(
-    Artifacts.open(address.name, address.revision)
-  )
+  const store = yield* Task.wait(IDBStore.open(address.name))
+
+  const artifact = yield* Task.wait(Artifacts.open(store, address.revision))
   const revision = yield* Task.wait(artifact.revision())
 
   return new ArtifactsStore(artifact, revision ?? GENESYS)
