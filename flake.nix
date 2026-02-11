852c9ebfb6af4197f0bbd59cb6795cc0d19caf8d
Irakli Gozalishvili <contact@gozala.io>
2026-02-10 23:23:49 -0800

    feat: migrate varsig crate into dialog

--- flake.nix
+++ flake.nix
@@ -134,7 +134,6 @@
               lockFile = ./Cargo.lock;
               outputHashes = {
                 "ucan-0.5.0" = "sha256-Rw4zUKEe2MM/BhNLw8DlP35uhbfD8N2ROFPmiTf1zxs=";
-                "varsig-0.1.0" = "sha256-Rw4zUKEe2MM/BhNLw8DlP35uhbfD8N2ROFPmiTf1zxs=";
               };
             };
           };
